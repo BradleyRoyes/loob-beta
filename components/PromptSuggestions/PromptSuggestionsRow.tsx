61c9b1ae712bf07632adbb893b6c9032234c063a
Bradley C Royes <bradroyes@gmail.com>
2024-02-03 17:39:49 +0100

    Update PromptSuggestionsRow.tsx

--- components/PromptSuggestions/PromptSuggestionsRow.tsx
+++ components/PromptSuggestions/PromptSuggestionsRow.tsx
@@ -2,7 +2,7 @@ import PromptSuggestionButton from "./PromptSuggestionButton";
 
 const PromptSuggestionRow = ({ onPromptClick }) => {
   const prompts = [
-    'How does similarity search work with a Vector DB?',
+    'How does penis search work with a Vector DB?',
     'What is DataStax Enterprise?',
     'How does CassIO work?',
     'What are some common FAQs about Astra?',
