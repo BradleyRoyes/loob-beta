38f84420c9fdd94103697fda698ca02fe1d4bf09
IvaTomevska <38501703+IvaTomevska@users.noreply.github.com>
2024-02-04 13:41:08 +0000

    radicle

--- components/mediarecorder.tsx
+++ components/mediarecorder.tsx
@@ -73,7 +73,7 @@ function AudioRecorder({ onTranscription }: AudioRecorderProps) {
     }
 
     try {
-      const response = await fetch('/api/chat/transcribe', {
+      const response = await fetch('/hooks/transcribe', {
         method: 'POST',
         body: audio,
         headers: {
