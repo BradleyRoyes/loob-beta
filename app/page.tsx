18e8e5b7cd2475777e1500e67085feb9217cb4ee
IvaTomevska <38501703+IvaTomevska@users.noreply.github.com>
2024-02-05 21:08:03 +0000

    try

--- app/page.tsx
+++ app/page.tsx
@@ -103,18 +103,18 @@ export default function Page() {
           </div>
           <PromptSuggestionRow onPromptClick={handlePrompt} />
           <AudioRecorder onTranscription={handleTranscription} />
-          <form className="flex gap-2" onSubmit={handleSend}>
-            <input
-              onChange={handleInputChange}
-              value={input}
-              className="flex-1 text-sm md:text-base rounded-md p-2"
-              placeholder="Send a message..."
-            />
-            <button type="submit" className="rounded-md px-2.5">
+          <form className='flex h-[40px] gap-2' onSubmit={handleSend}>
+            <input onChange={handleInputChange} value={input} className='chatbot-input flex-1 text-sm md:text-base outline-none bg-transparent rounded-md p-2' placeholder='Send a message...' />
+            <button
+              type="submit"
+              className="chatbot-send-button flex rounded-md items-center justify-center px-2.5 origin:px-3"
+            >
               <svg width="20" height="20" viewBox="0 0 20 20">
                 <path d="M2.925 5.025L9.18333 7.70833L2.91667 6.875L2.925 5.025ZM9.175 12.2917L2.91667 14.975V13.125L9.175 12.2917ZM1.25833 2.5L1.25 8.33333L13.75 10L1.25 11.6667L1.25833 17.5L18.75 10L1.25833 2.5Z" />
               </svg>
-              <span className="hidden md:block font-semibold">Send</span>
+              <span className="hidden origin:block font-semibold text-sm ml-2">
+                Send
+              </span>
             </button>
           </form>
           <Footer />
