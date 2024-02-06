c420210c6831d02dea7e1e64ddf98d7ec91856cb
IvaTomevska <38501703+IvaTomevska@users.noreply.github.com>
2024-02-06 20:42:52 +0000

    new icon

--- components/AudioRecorder.tsx
+++ components/AudioRecorder.tsx
@@ -67,15 +67,13 @@ const AudioRecorder: React.FC<AudioRecorderProps> = ({ onTranscription }) => {
         {/* SVG Microphone Icon */}
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
+          width="24"
+          height="24"
           fill="currentColor"
           viewBox="0 0 16 16"
-          className="bi bi-mic"
         >
-          <path d="M11 2a2 2 0 1 1-4 0 2 2 0 0 1 4 0z" />
-          <path d="M11 7V5a3 3 0 1 0-6 0v2a3 3 0 1 0 6 0z" />
-          <path d="M6.5 10.5a.5.5 0 0 1 .5.5V14h.5a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1h.5v-3a.5.5 0 0 1 .5-.5z" />
+          <path d="M7.5 1a2.5 2.5 0 0 0-2.5 2.5v5a2.5 2.5 0 0 0 5 0v-5A2.5 2.5 0 0 0 7.5 1zm-1 2.5a1 1 0 1 1 2 0v5a1 1 0 1 1-2 0v-5z" />
+          <path d="M4.81 8.157c-.577-.58-1.135-.862-1.616-.927a.5.5 0 0 0-.388.806c.226.227.538.572.878 1.025C4.395 10.042 5 11.57 5 13v1a.5.5 0 0 0 1 0v-1c0-1.43.605-2.958 1.315-4.939.34-.453.652-.798.878-1.025a.5.5 0 0 0-.388-.806c-.481.065-1.039.347-1.616.927a.5.5 0 0 1-.708 0zM8 14.5a2 2 0 1 1-4 0 .5.5 0 0 1 1 0 1 1 0 1 0 2 0 .5.5 0 0 1 1 0z" />
         </svg>
       </button>
       {error && <p className="text-red-500 text-xs mt-2">Error: {error}</p>}
