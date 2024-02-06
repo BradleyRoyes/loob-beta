9c69585c7291058af0fbc606b149c6aadeb865a6
BradleyRoyes <bradroyes@gmail.com>
2024-02-06 13:49:33 +0000

    hipes

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,69 +1,71 @@
 import OpenAI from 'openai';
 import { OpenAIStream, StreamingTextResponse } from 'ai';
 import { AstraDB } from "@datastax/astra-db-ts";
-import { v4 as uuidv4 } from 'uuid'; // Import the uuid library
+import { v4 as uuidv4 } from 'uuid';
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-const astraDb = new AstraDB(process.env.ASTRA_DB_APPLICATION_TOKEN, process.env.ASTRA_DB_ENDPOINT, process.env.ASTRA_DB_NAMESPACE);
-
-// Function to generate a unique session ID if it doesn't exist
-function generateSessionId(req) {
-  const existingSessionId = req.headers.get('x-session-id');
-  if (existingSessionId) {
-    return existingSessionId;
-  } else {
-    const newSessionId = uuidv4();
-    req.headers.set('x-session-id', newSessionId);
-    return newSessionId;
-  }
-}
+const astraDb = new AstraDB(
+  process.env.ASTRA_DB_APPLICATION_TOKEN,
+  process.env.ASTRA_DB_ENDPOINT,
+  process.env.ASTRA_DB_NAMESPACE
+);
 
 export async function POST(req) {
   try {
     const { messages, useRag, llm, similarityMetric } = await req.json();
 
-    const latestMessage = messages[messages?.length - 1]?.content;
+    // Check if a session ID is provided in the request headers, or generate a new one
+    let sessionId = req.headers.get("x-session-id");
+    if (!sessionId) {
+      sessionId = uuidv4();
+      req.headers.set("x-session-id", sessionId);
+    }
 
     let docContext = '';
     if (useRag) {
-      const { data } = await openai.embeddings.create({ input: latestMessage, model: 'text-embedding-ada-002' });
+      const latestMessage = messages[messages.length - 1]?.content;
 
-      const collection = await astraDb.collection(`chat_${similarityMetric}`);
+      if (latestMessage) {
+        // Generate embeddings for the latest message
+        const { data } = await openai.embeddings.create({
+          input: latestMessage,
+          model: 'text-embedding-ada-002',
+        });
 
-      const cursor = collection.find(null, {
-        sort: {
-          $vector: data[0]?.embedding,
-        },
-        limit: 5,
-      });
-
-      const documents = await cursor.toArray();
-
-      docContext = `
-        START CONTEXT
-        ${documents?.map(doc => doc.content).join("\n")}
-        END CONTEXT
-      `
+        // Retrieve similar documents from AstraDB based on embeddings
+        const collection = await astraDb.collection(`chat_${similarityMetric}`);
+        const cursor = collection.find(null, {
+          sort: {
+            $vector: data[0]?.embedding,
+          },
+          limit: 5,
+        });
+        const documents = await cursor.toArray();
+        docContext = documents.map((doc) => doc.content).join("\n");
+      }
     }
+
     const ragPrompt = [
       {
         role: 'system',
-        content: `You are an AI assistant designed to guide people through their transformative psychedelic trip experiences. Be compassionate and curious, engaging users to share more about their experiences.
+        content: `You are an AI assistant designed to guide people through their transformative psychedelic trip experiences. Be compassionate and curious, engaging users to share more about their experiences'.
         ${docContext} 
         If the answer is not provided in the context, the AI assistant will say, "I'm sorry, I don't know the answer".
       `,
       },
     ]
 
-    // Send all user inputs to the "journey_journals" collection with a session ID
-    const sessionId = generateSessionId(req);
+    // Send all user inputs to the "journey_journals" collection
     for (const message of messages) {
       if (message.role === 'user') {
         const collection = await astraDb.collection("journey_journal");
-        await collection.insertOne({ ...message, sessionId });
+        await collection.insertOne({
+          ...message,
+          sessionId: sessionId,
+        });
       }
     }
 
