907ddddc5e723045b2519995dbbc086a86a41892
IvaTomevska <38501703+IvaTomevska@users.noreply.github.com>
2024-02-06 19:43:50 +0000

    again

--- app/page.tsx
+++ app/page.tsx
@@ -38,6 +38,7 @@ export default function Page() {
       const { scrollHeight, clientHeight } = chatContainerRef.current;
       chatContainerRef.current.scrollTop = scrollHeight - clientHeight;
     }
+  };
 
   useLayoutEffect(() => {
     scrollToBottom();
@@ -82,7 +83,10 @@ export default function Page() {
 
   return (
     <main className="flex h-screen flex-col items-center justify-center">
-      <section ref={chatContainerRef} className="chatbot-section flex flex-col origin:w-[800px] w-full origin:h-[735px] h-full rounded-md p-2 md:p-6">
+      <section
+        ref={chatContainerRef}
+        className="chatbot-section flex flex-col origin:w-[800px] w-full origin:h-[735px] h-full rounded-md p-2 md:p-6"
+      >
         <div className="chatbot-header pb-6">
           <div className="flex justify-between items-center">
             <h1 className="chatbot-text-primary text-6xl md:text-7xl font-extrabold tracking-wide">
