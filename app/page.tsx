1e70d308110a0a279b651fea2d227ee4a492b326
IvaTomevska <38501703+IvaTomevska@users.noreply.github.com>
2024-02-06 21:10:08 +0000

    going back on it

--- app/page.tsx
+++ app/page.tsx
@@ -132,9 +132,9 @@ export default function Page() {
           </div>
         </div>
         {/* <PromptSuggestionRow onPromptClick={handlePrompt} /> */}
-
+       
         <form className="flex h-[40px] gap-2" onSubmit={handleSend}>
-          <AudioRecorder onTranscription={handleTranscription} currentInputValue={input} />
+          <AudioRecorder onTranscription={handleTranscription} />
           <input
             onChange={handleInputChange}
             value={input}
