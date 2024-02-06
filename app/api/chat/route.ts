2414381a76e57ab80c6711ecae86246b9e3b1fc6
BradleyRoyes <bradroyes@gmail.com>
2024-02-06 12:58:28 +0000

    sample content

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -10,7 +10,7 @@ const astraDb = new AstraDB(process.env.ASTRA_DB_APPLICATION_TOKEN, process.env.
 
 export async function POST(req: Request) {
   try {
-    const { messages, useRag, llm, similarityMetric } = await req.json();
+    const { messages, useRag, llm, similarityMetric, sessionId } = await req.json(); // Assuming sessionId is now part of the request payload
 
     const latestMessage = messages[messages?.length - 1]?.content;
 
@@ -29,37 +29,38 @@ export async function POST(req: Request) {
 
       const documents = await cursor.toArray();
 
+      // Use documents to form context for ChatGPT responses
       docContext = `
         START CONTEXT
         ${documents?.map(doc => doc.content).join("\n")}
         END CONTEXT
-      `
+      `;
     }
+
+    // Adjust the content of ragPrompt to match your application's context and requirements
     const ragPrompt = [
       {
         role: 'system',
-        content: `You are an AI assistant designed to guide people through their transformative psychedelic trip expereiences. Be compassionate and curious, engaging users to share mroe about their experiences'.
+        content: `You are an AI assistant designed to guide people through their transformative psychedelic trip experiences. Be compassionate and curious, engaging users to share more about their experiences.
         ${docContext} 
-        If the answer is not provided in the context, the AI assistant will say, "I'm sorry, I don't know the answer".
-      `,
+        If the answer is not provided in the context, the AI assistant will say, "I'm sorry, I don't know the answer".`
       },
-    ]
+    ];
 
-    // Send all user inputs to the "journey_journals" collection
+    // Insert each user message into the "journey_journal" collection with sessionId
     for (const message of messages) {
       if (message.role === 'user') {
         const collection = await astraDb.collection("journey_journal");
-        await collection.insertOne(message); // Assuming 'message' is the user input data
+        await collection.insertOne({ ...message, sessionId }); // Add sessionId to each message
       }
     }
 
-    const response = await openai.chat.completions.create(
-      {
-        model: llm ?? 'gpt-3.5-turbo',
-        stream: true,
-        messages: [...ragPrompt, ...messages],
-      }
-    );
+    const response = await openai.chat.completions.create({
+      model: llm ?? 'gpt-3.5-turbo',
+      stream: true,
+      messages: [...ragPrompt, ...messages], // Combine ragPrompt and messages for the ChatGPT prompt
+    });
+
     const stream = OpenAIStream(response);
     return new StreamingTextResponse(stream);
   } catch (e) {
