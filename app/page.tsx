f4bafa5be48f40946ba0f776f0a76af560970607
IvaTomevska <38501703+IvaTomevska@users.noreply.github.com>
2024-02-07 22:10:49 +0000

    try

--- app/page.tsx
+++ app/page.tsx
@@ -132,7 +132,7 @@ export default function Page() {
         </div>
         {/* <PromptSuggestionRow onPromptClick={handlePrompt} /> */}
         <div className="flex items-center justify-between gap-2">
-          <AudioRecorder onTranscription={handleTranscription} />
+          {/* <AudioRecorder onTranscription={handleTranscription} /> */}
           <form className="flex flex-1 gap-2" onSubmit={handleSend}>
             <input
               onChange={handleInputChange}
