421a489ba2162453840fa7a5488079d6d5ef3aef
Bradley C Royes <bradroyes@gmail.com>
2024-02-08 20:24:14 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -138,7 +138,7 @@ export default function Page() {
             </div>
           </div>
           <p className="chatbot-text-secondary-inverse text-lg md:text-xl mt-2 md:mt-4 font-medium">
-            Welcome to Loob Labratories.
+            Journaling done differently
           </p>
         </div>
         <div className="flex-1 relative overflow-y-auto my-4 md:my-6">
