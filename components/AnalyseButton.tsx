cadbd3e3fca0e4c5fd54962657b2ea76188dfd83
Bradley C Royes <bradroyes@gmail.com>
2024-02-09 21:49:42 +0100

    Update AnalyseButton.tsx

--- components/AnalyseButton.tsx
+++ components/AnalyseButton.tsx
@@ -1,16 +1,13 @@
 import React from "react";
-import { useChat } from "ai/react";
 
-const ThemeButton: React.FC = () => {
-  const { append } = useChat();
+interface AnalyseButtonProps {
+  onClick: () => void;
+}
 
-  const handleAnalyse = () => {
-    append("**analyse our conversation**");
-  };
-
+const AnalyseButton: React.FC<AnalyseButtonProps> = ({ onClick }) => {
   return (
     <button
-      onClick={handleAnalyse}
+      onClick={onClick}
       className="button-dash flex rounded-md items-center justify-center px-2.5"
       style={{ fontWeight: "500" }}
     >
@@ -19,4 +16,4 @@ const ThemeButton: React.FC = () => {
   );
 };
 
-export default ThemeButton;
+export default AnalyseButton;
