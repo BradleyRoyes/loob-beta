e924543de507791c8cdc82d52b86379a9663eed1
IvaTomevska <38501703+IvaTomevska@users.noreply.github.com>
2024-02-09 14:03:49 +0000

    try

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -16,6 +16,7 @@ const Footer = () => {
         <svg
           version="1.1"
           id="Layer_1"
+          xmlns="http://www.w3.org/2000/svg"
           x="0px"
           y="0px"
           viewBox="0 0 595.3 841.9"
@@ -23,65 +24,29 @@ const Footer = () => {
         >
           <path
             className="st0"
-            style={{
-              fill: "none",
-              stroke: "#000000",
-              strokeWidth: 9,
-              strokeMiterlimit: 10,
-            }}
             d="M286.6,280.3l-3.7-72.2c0,0-61.2,20.3-74,32.2c-20.2,18.9-12.3,43.8-9.6,63.6c2.7,19.8,21.8,79.6-33.9,79.5
-                c-57.6-0.1-53.4-70.2-53.4-70.2"
+          c-57.6-0.1-53.4-70.2-53.4-70.2"
           />
           <path
             className="st0"
-            style={{
-              fill: "none",
-              stroke: "#000000",
-              strokeWidth: 9,
-              strokeMiterlimit: 10,
-            }}
             d="M69.8,485.8h135.6c0,0-3.6-72.6-70.2-72.6C69.8,413.2,69.8,485.8,69.8,485.8z"
           />
           <path
             className="st0"
-            style={{
-              fill: "none",
-              stroke: "#000000",
-              strokeWidth: 9,
-              strokeMiterlimit: 10,
-            }}
             d="M69.8,488.9c0,0,0,43.9,32,67.7c50.4,37.4,102.6-15.5,102.6-15.5v28.7"
           />
           <polyline
             className="st1"
-            style={{
-              fill: "none",
-              stroke: "#000000",
-              strokeWidth: 16,
-              strokeMiterlimit: 10,
-            }}
             points="245.2,280.3 245.2,421.4 245.2,600 "
           />
           <path
             className="st1"
-            style={{
-              fill: "none",
-              stroke: "#000000",
-              strokeWidth: 16,
-              strokeMiterlimit: 10,
-            }}
             d="M364.3,339.6c0,0-48.1,60.4-108.7,81.8l184,173.4"
           />
           <path
             className="st0"
-            style={{
-              fill: "none",
-              stroke: "#000000",
-              strokeWidth: 9,
-              strokeMiterlimit: 10,
-            }}
             d="M478.1,384.5c0,0-9.6-23.3-49-23.3c-39,0-54.2,18.4-54.2,35.9c0,17.5,15.6,30.1,53.5,31.7
-                c69.8,2.9,61.3,73.1,0.9,73.1c-54.5,0-60.3-28.1-60.3-28.1"
+          c69.8,2.9,61.3,73.1,0.9,73.1c-54.5,0-60.3-28.1-60.3-28.1"
           />
         </svg>
       </div>
