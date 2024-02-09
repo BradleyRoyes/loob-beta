a7232da06a8adf8bdac7bda331cb9714fcee579f
IvaTomevska <38501703+IvaTomevska@users.noreply.github.com>
2024-02-09 19:54:33 +0000

    ttttt

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -31,7 +31,7 @@ const Footer = () => {
               } as any
             }
           >
-            <g transform="translate(0, -73)">
+            <g transform="translate(10, -73)">
               <path
                 className="st0"
                 style={{
@@ -102,7 +102,7 @@ const Footer = () => {
               />
             </g>
             <circle
-              cx="241.5"
+              cx="271.5"
               cy="336.5"
               r="300"
               style={{
