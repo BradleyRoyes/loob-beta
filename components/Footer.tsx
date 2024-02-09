9a8b2980ac54719e78183fc9ff12f9206667367b
IvaTomevska <38501703+IvaTomevska@users.noreply.github.com>
2024-02-09 14:06:37 +0000

    try

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -16,7 +16,8 @@ const Footer = () => {
         <svg
           version="1.1"
           id="Layer_1"
-          xmlns="http://www.w3.org/2000/svg"
+          //xmlns="http://www.w3.org/2000/svg"
+          xmlns="http://www.w3.org/1999/xlink"
           x="0px"
           y="0px"
           viewBox="0 0 595.3 841.9"
