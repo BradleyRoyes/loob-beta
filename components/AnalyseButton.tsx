e1b5207f1264d8d67e382350b947ac7beb9aca5e
Bradley C Royes <bradroyes@gmail.com>
2024-02-09 22:03:30 +0100

    Update AnalyseButton.tsx

--- components/AnalyseButton.tsx
+++ components/AnalyseButton.tsx
@@ -1,21 +1,14 @@
+// AnalyseButton.js
 import React from "react";
 
-interface AnalyseButtonProps {
-  onClick: () => void;
-}
-
-const AnalyseButton: React.FC<AnalyseButtonProps> = ({ onClick }) => {
- const handleAnalyseButtonClick = () => {
-  const analyseMessage = "analyse our conversation until now";
-  append({ author: "user", content: analyseMessage }); // Send the message to the chat
-  // Optionally, you can also clear the input field after sending the message
-  handleInputChange({ target: { value: "" } } as React.ChangeEvent<HTMLInputElement>);
-};
-
+const AnalyseButton = ({ onClick }) => {
+  const handleButtonClick = () => {
+    onClick("Analyse our conversation until now"); // Pass the predefined message to the onClick handler
+  };
 
   return (
     <button
-      onClick={handleAnalyseButtonClick} // Use the local handler function
+      onClick={handleButtonClick}
       className="button-dash flex rounded-md items-center justify-center px-2.5"
       style={{ fontWeight: "500" }}
     >
