21d62d06ca7bcfb9f1cbb7ffb32320fcaf73b9b9
IvaTomevska <38501703+IvaTomevska@users.noreply.github.com>
2024-02-09 18:21:15 +0000

    po navika

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -24,8 +24,8 @@ const Footer = () => {
           style={
             {
               enableBackground: "new 0 0 513 713",
-              width: "20px", // Adjust as needed
-              height: "50px", // Adjust as needed
+              width: "50px", // Adjust as needed
+              height: "75px", // Adjust as needed
               marginLeft: "4px", // Ensures spacing between text and SVG
             } as any
           }
@@ -35,7 +35,7 @@ const Footer = () => {
             style={{
               fill: "none",
               stroke: "#6B6F73",
-              strokeWidth: 9,
+              strokeWidth: 16,
               strokeMiterlimit: 10,
             }}
             d="M286.6,280.3l-3.7-72.2c0,0-61.2,20.3-74,32.2c-20.2,18.9-12.3,43.8-9.6,63.6c2.7,19.8,21.8,79.6-33.9,79.5
@@ -47,7 +47,7 @@ const Footer = () => {
             style={{
               fill: "none",
               stroke: "#6B6F73",
-              strokeWidth: 9,
+              strokeWidth: 16,
               strokeMiterlimit: 10,
             }}
             d="M69.8,485.8h135.6c0,0-3.6-72.6-70.2-72.6C69.8,413.2,69.8,485.8,69.8,485.8z"
@@ -58,7 +58,7 @@ const Footer = () => {
             style={{
               fill: "none",
               stroke: "#6B6F73",
-              strokeWidth: 9,
+              strokeWidth: 16,
               strokeMiterlimit: 10,
             }}
             d="M69.8,488.9c0,0,0,43.9,32,67.7c50.4,37.4,102.6-15.5,102.6-15.5v28.7"
@@ -69,7 +69,7 @@ const Footer = () => {
             style={{
               fill: "none",
               stroke: "#6B6F73",
-              strokeWidth: 16,
+              strokeWidth: 24,
               strokeMiterlimit: 10,
             }}
             points="245.2,280.3 245.2,421.4 245.2,600 "
@@ -80,7 +80,7 @@ const Footer = () => {
             style={{
               fill: "none",
               stroke: "#6B6F73",
-              strokeWidth: 16,
+              strokeWidth: 24,
               strokeMiterlimit: 10,
             }}
             d="M364.3,339.6c0,0-48.1,60.4-108.7,81.8l184,173.4"
@@ -91,7 +91,7 @@ const Footer = () => {
             style={{
               fill: "none",
               stroke: "#6B6F73",
-              strokeWidth: 9,
+              strokeWidth: 16,
               strokeMiterlimit: 10,
             }}
             d="M478.1,384.5c0,0-9.6-23.3-49-23.3c-39,0-54.2,18.4-54.2,35.9c0,17.5,15.6,30.1,53.5,31.7
