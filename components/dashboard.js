1cc450f9760792c14c1bac1f4d8f511800adc9ca
IvaTomevska <38501703+IvaTomevska@users.noreply.github.com>
2024-02-10 13:44:05 +0000

    try

--- components/dashboard.js
+++ components/dashboard.js
@@ -133,7 +133,7 @@ const Dashboard = () => {
       <section className="chatbot-section max-w-4xl w-full overflow-hidden rounded-md shadow-lg">
         <div className="p-4">
           <div className="flex justify-between items-center mb-6">
-            <h1 className="chatbot-text-primary items-center justify-center h-screen text-3xl font-bold">
+            <h1 className="chatbot-text-primary text-3xl font-bold">
               Dashboard
             </h1>
             <ThemeButton theme={theme} setTheme={setTheme} />
