8814551fff52d2550474943f0ca43e8a64a4a1a4
IvaTomevska <38501703+IvaTomevska@users.noreply.github.com>
2024-02-12 17:19:20 +0000

    sample prompt

--- components/PromptSuggestions/PromptSuggestionsRow.tsx
+++ components/PromptSuggestions/PromptSuggestionsRow.tsx
@@ -1,17 +1,16 @@
 import PromptSuggestionButton from "./PromptSuggestionButton";
 
 const PromptSuggestionRow = ({ onPromptClick }) => {
-  const prompts = [
-    'What is your purpose?',
-    'What is Loob?',
-    'How do you work?',
-    'How can I best share an experience with you?',
-  ];
+  const prompts = ["Upcoming events at MOOS"];
 
   return (
     <div className="flex flex-row flex-wrap justify-start items-center py-4 gap-2">
       {prompts.map((prompt, index) => (
-        <PromptSuggestionButton key={`suggestion-${index}`} text={prompt} onClick={() => onPromptClick(prompt)} />
+        <PromptSuggestionButton
+          key={`suggestion-${index}`}
+          text={prompt}
+          onClick={() => onPromptClick(prompt)}
+        />
       ))}
     </div>
   );
