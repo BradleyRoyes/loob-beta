ac0ab2359621033ba027cea274082e1bf3aa2af0
kaydee <38501703+IvaTomevska@users.noreply.github.com>
2024-02-23 16:31:04 +0000

    back

--- components/dashboard.js
+++ components/dashboard.js
@@ -1,19 +1,17 @@
 import React, { useEffect, useRef } from "react";
-import { useAnalysisData } from "../components/AnalysisDataContext";
+
 const Dashboard = () => {
-  // const { analysisData } = useAnalysisData(); // Use the context to access analysis data
   const canvasRef = useRef(null);
   const points = useRef([]);
   const maxNodes = 10;
   const connectionDistance = 100;
-  // useEffect(() => {
-  //   console.log("Received analysis data:", analysisData); // Log the analysis data to verify receipt
-  // }, [analysisData]); // Add analysisData as a dependency to useEffect to re-run when analysisData changes
+
   useEffect(() => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+
     // Initialize points with position, velocity, and radius
     for (let i = 0; i < maxNodes; i++) {
       points.current.push({
@@ -24,6 +22,7 @@ const Dashboard = () => {
         radius: Math.random() * 2 + 1,
       });
     }
+
     const draw = () => {
       ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear canvas
       updatePoints(); // Update points' positions based on their velocities
@@ -31,18 +30,22 @@ const Dashboard = () => {
       drawConnections(ctx); // Draw connections between close points
       requestAnimationFrame(draw); // Create an animation loop
     };
+
     draw();
-  }); // Add analysisData as a dependency to useEffect to re-run when analysisData changes
+  }, []);
+
   // Update points' positions
   const updatePoints = () => {
     points.current.forEach((point) => {
       point.x += point.vx;
       point.y += point.vy;
+
       // Reverse velocity if the point hits the canvas boundary
       if (point.x <= 0 || point.x >= canvasRef.current.width) point.vx *= -1;
       if (point.y <= 0 || point.y >= canvasRef.current.height) point.vy *= -1;
     });
   };
+
   // Draw points
   const drawPoints = (ctx) => {
     points.current.forEach((point) => {
@@ -51,6 +54,7 @@ const Dashboard = () => {
       ctx.fill();
     });
   };
+
   // Draw connections between close points
   const drawConnections = (ctx) => {
     points.current.forEach((point, index) => {
@@ -66,6 +70,7 @@ const Dashboard = () => {
       }
     });
   };
+
   return (
     <canvas
       ref={canvasRef}
@@ -73,4 +78,5 @@ const Dashboard = () => {
     ></canvas>
   );
 };
+
 export default Dashboard;
