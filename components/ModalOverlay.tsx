d76e6654cd630ecd028a08de4055cb903a192fbe
Bradley C Royes <bradroyes@gmail.com>
2024-02-23 12:25:51 +0000

    update to page with modal

---
+++ components/ModalOverlay.tsx
@@ -0,0 +1,20 @@
+// ModalOverlay.tsx
+import React from "react";
+
+const ModalOverlay = ({ onClose }) => {
+  return (
+    <div className="modal-overlay">
+      <div className="modal-content">
+        <p>
+          To be relevant in a living system is to generate vitality. What is
+          that? It's relationships that build relationships that build
+          relationships: 3rd & 4th order relational process is real systemic
+          work. No KPI can measure it. This is #WarmData
+        </p>
+        <button onClick={onClose}>New Chat</button>
+      </div>
+    </div>
+  );
+};
+
+export default ModalOverlay;
