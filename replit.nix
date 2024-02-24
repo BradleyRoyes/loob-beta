8e04004944c614d1283d5844731201d391bf17d1
Bradley C Royes <bradroyes@gmail.com>
2024-02-24 12:19:28 +0100

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -2,7 +2,7 @@
     deps = [
         pkgs.yarn
         pkgs.esbuild
-        pkgs.nodejs-18_x
+        pkgs.nodejs-20_x
 
         pkgs.nodePackages.typescript
         pkgs.nodePackages.typescript-language-server
