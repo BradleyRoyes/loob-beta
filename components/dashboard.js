6213e2bfc9ffc6c15ab344258c47357a31201d8a
kaydee <38501703+IvaTomevska@users.noreply.github.com>
2024-02-24 16:43:27 +0000

    permanent line error fix

--- components/dashboard.js
+++ components/dashboard.js
@@ -185,25 +185,26 @@ const Dashboard = () => {
 
   const updatePoints = (noiseGen) => {
     points.current.forEach((point, index) => {
-      // Check if the current point is part of a permanent connection
-      const isPartOfPermanentConnection = permanentConnections.some(
-        (connection) => {
-          const [start, end] = connection.split("-").map(Number);
-          return start === index || end === index;
-        },
-      );
+      // Initially assume the point is not part of a permanent connection
+      let isPartOfPermanentLine = false;
 
-      if (!isPartOfPermanentConnection) {
-        // Apply Perlin noise for natural movement if not part of a permanent connection
+      // Check if the current point's index is in the permanentLine array
+      if (permanentLine.includes(index)) {
+        isPartOfPermanentLine = true;
+      }
+
+      if (!isPartOfPermanentLine) {
+        // Apply Perlin noise for natural movement if not part of a permanent line
         const noiseX = noiseGen.simplex2(point.x * 0.01, point.y * 0.01);
         const noiseY = noiseGen.simplex2(point.y * 0.01, point.x * 0.01);
 
         point.vx += noiseX * 0.03; // Adjust velocity based on Perlin noise
         point.vy += noiseY * 0.03;
       } else {
-        // Optionally, apply a different logic for points that are part of a permanent connection
-        // For example, you might want to reduce the velocity to make the movement less pronounced
-        point.vx *= 0.95; // Slow down the velocity by a certain factor
+        // For points that are part of the permanent line, you might want to
+        // apply a different logic or skip the update to maintain the line integrity
+        // For now, let's slightly reduce their velocity to demonstrate this concept
+        point.vx *= 0.95;
         point.vy *= 0.95;
       }
 
@@ -212,8 +213,12 @@ const Dashboard = () => {
       point.y += point.vy;
 
       // Boundary check to reverse the velocity if the point hits the canvas edge
-      if (point.x <= 0 || point.x >= canvasRef.current.width) point.vx *= -1;
-      if (point.y <= 0 || point.y >= canvasRef.current.height) point.vy *= -1;
+      if (point.x <= 0 || point.x >= canvasRef.current.width) {
+        point.vx *= -1;
+      }
+      if (point.y <= 0 || point.y >= canvasRef.current.height) {
+        point.vy *= -1;
+      }
 
       // Manage the trail for visual effect
       point.trail.push({ x: point.x, y: point.y });
