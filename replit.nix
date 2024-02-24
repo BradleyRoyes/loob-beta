ba7edde6ad7d7a6f467df2ff7aa8a142338b757a
Bradley C Royes <bradroyes@gmail.com>
2024-02-24 14:08:06 +0100

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -2,7 +2,7 @@
     deps = [
         pkgs.yarn
         pkgs.esbuild
-        (pkgs.nodejs-18_x.override { version = "18.17.0"; })
+        pkgs.nodejs-18_x
 
         pkgs.nodePackages.typescript
         pkgs.nodePackages.typescript-language-server
