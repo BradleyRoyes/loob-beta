1c260ac37dd46923b3ec2ec0aea2fb796e98d8b2
Bradley C Royes <bradroyes@gmail.com>
2024-02-25 15:59:08 +0000

    route

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -161,7 +161,7 @@ export async function POST(req: any) {
       ];
     
     const response = await openai.chat.completions.create({
-      model: llm ?? "gpt-4",
+      model: llm ?? "gpt-3.5-turbo",
       stream: true,
       messages: [...ragPrompt, ...messages],
     });
