db4a814605739fd0f678faa274c926383c90c7a5
seks.design <bradroyes@gmail.com>
2024-03-16 15:07:36 +0100

    Update SplashScreen.tsx

--- components/SplashScreen.tsx
+++ components/SplashScreen.tsx
@@ -43,8 +43,9 @@ const SplashScreen: React.FC<{ onEnter: (prompt?: string) => void }> = ({
       {phase === "introduction" && (
         <motion.div className="content" variants={variants}>
           <h1 className="gradientText" style={{ fontSize: 'normal' }}>
-         I am loob, a listener. <br/> An urban story container for you to tell new stories about new experiences. <br/> 
-<br/>Movement is everything,<br/> nothing is the goal.  
+         I’m loob.  <br/>I am a listener. <br/> An urban story container for you to tell new stories about new experiences. <br/> Come inside, lets go downstream.  
+
+Movement is everything, nothing is the goal.  
           </h1>
           <button onClick={() => proceed("learnMore")}>
             Continue
@@ -52,27 +53,26 @@ const SplashScreen: React.FC<{ onEnter: (prompt?: string) => void }> = ({
         </motion.div>
       )}
 
-{phase === "learnMore" && (
-  <motion.div className="content" variants={variants}>
-    <h1 className="gradientText">Would you like to</h1>
-    <div className="priorityButton">
-      <button onClick={() => onEnter("I would like to share an experience I am having with you.")}>
-        Share an Experience
-      </button>
-    </div>
-    <div className="rowButtons">
-      <button onClick={() => onEnter("Tell me about MOOS.")}>
-        Learn about MOOS
-      </button>
-      <button onClick={() => onEnter("Tell me about EDS and seks/loob.")}>
-        Learn about EDS
-      </button>
-      <button onClick={() => onEnter("I am having a difficult time, can you give me harm reduction support")}>
-        Harm Reduction
-      </button>
-    </div>
-  </motion.div>
-)}
+
+      {phase === "learnMore" && (
+      
+        <motion.div className="content" variants={variants}>
+          <h1 className="gradientText">Would you like to</h1>
+          <button onClick={() => onEnter("I would like to share an experience I am having with you.")}>
+            Share an Experience
+          </button>
+          <button onClick={() => onEnter("Tell me about MOOS.")}>
+            Learn about MOOS
+          </button>
+          <button onClick={() => onEnter("Tell me about EDS and seks/loob.")}>
+            Learn about EDS
+          </button>
+         
+          <button onClick={() => onEnter("I am having a difficult time, can you give me harm reduction support")}>
+            Harm Reduction
+          </button>
+        </motion.div>
+      )}
 
       {phase === "feedback" && (
         <motion.div className="content" variants={variants}>
