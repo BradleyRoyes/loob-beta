50c10e73f804354133ad6b897d138345fb4a7215
seks.design <bradroyes@gmail.com>
2024-05-17 10:43:19 +0200

    Update AudioRecorder.tsx

--- components/AudioRecorder.tsx
+++ components/AudioRecorder.tsx
@@ -70,7 +70,7 @@ const AudioRecorder: React.FC<AudioRecorderProps> = ({ onRecordingComplete, star
         <>
           <path d="M12 1a3 3 0 0 1 3 3v6a3 3 0 0 1-6 0V4a3 3 0 0 1 3-3z" />
           <path d="M19 10v2a7 7 0 0 1-14 0v-2" />
-          <line x1="12" y="19" x2="12" y2="23" />
+          <line x1="12" y1="19" x2="12" y2="23" />
           <line x1="8" y1="23" x2="16" y2="23" />
         </>
       )}
