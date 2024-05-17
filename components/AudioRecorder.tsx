f9e9178ae8a58422a72d7283be1f69e998a28180
Bradley C Royes <bradley@Bradleys-MacBook-Pro.local>
2024-05-17 09:50:43 +0200

    new changes

--- components/AudioRecorder.tsx
+++ components/AudioRecorder.tsx
@@ -1,15 +1,14 @@
 import React, { useState } from 'react';
 
-const AudioRecorder = ({ onRecordingComplete }) => {
+const AudioRecorder = ({ onRecordingComplete, startRecording }) => {
   const [recording, setRecording] = useState(false);
   const [mediaRecorder, setMediaRecorder] = useState<MediaRecorder | null>(null);
 
-  const startRecording = async () => {
+  const startAudioRecording = async () => {
     if (recording) return;
     try {
       const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
       const newMediaRecorder = new MediaRecorder(stream);
-      // Explicitly type audioChunks as an array of Blob objects
       let audioChunks: Blob[] = [];
 
       newMediaRecorder.ondataavailable = event => {
@@ -25,19 +24,20 @@ const AudioRecorder = ({ onRecordingComplete }) => {
       newMediaRecorder.start();
       setMediaRecorder(newMediaRecorder);
       setRecording(true);
+      startRecording(); // Trigger the start recording animation
     } catch (error) {
       console.error('Error starting recording:', error);
     }
   };
 
-  const stopRecording = () => {
+  const stopAudioRecording = () => {
     if (!recording || !mediaRecorder) return;
     mediaRecorder.stop();
     setRecording(false);
   };
 
   const buttonStyle = {
-    backgroundColor: 'transparent', // Pastel blue when recording
+    backgroundColor: 'transparent',
     border: '2px solid var(--text-primary-inverse)',
     borderRadius: '50%',
     cursor: 'pointer',
@@ -48,23 +48,20 @@ const AudioRecorder = ({ onRecordingComplete }) => {
     transition: 'all 0.3s',
   };
 
-  
   const MicIcon = () => (
     <svg
       viewBox="0 0 24 24"
       width="24"
       height="24"
-      stroke={recording ? 'none' : "var(--text-primary-inverse)"} // No stroke when recording
+      stroke={recording ? 'none' : "var(--text-primary-inverse)"}
       strokeWidth="2"
-      fill={recording ? '#ff8e88' : "none"} // Fill red when recording, otherwise no fill
+      fill={recording ? '#ff8e88' : "none"}
       strokeLinecap="round"
       strokeLinejoin="round"
     >
       {recording ? (
-        // When recording, show a red square in the center
         <rect x="5" y="5" width="14" height="14" fill="#d32f2f" rx="3" />
       ) : (
-        // Default microphone icon
         <>
           <path d="M12 1a3 3 0 0 1 3 3v6a3 3 0 0 1-6 0V4a3 3 0 0 1 3-3z" />
           <path d="M19 10v2a7 7 0 0 1-14 0v-2" />
@@ -78,7 +75,7 @@ const AudioRecorder = ({ onRecordingComplete }) => {
   return (
     <div>
       <button className="recordButton"
-        onClick={recording ? stopRecording : startRecording}
+        onClick={recording ? stopAudioRecording : startAudioRecording}
         style={buttonStyle}
       >
         <MicIcon />
