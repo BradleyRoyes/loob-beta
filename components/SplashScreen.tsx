bd0b8c9bb841a69cb4ebac20ee73df039e220dae
seks.design <bradroyes@gmail.com>
2024-05-17 10:17:34 +0200

    Update SplashScreen.tsx

--- components/SplashScreen.tsx
+++ components/SplashScreen.tsx
@@ -7,23 +7,29 @@ const SplashScreen: React.FC<{ onEnter: (prompt?: string) => void }> = ({
   onEnter,
 }) => {
   const [phase, setPhase] = useState<string>("welcome");
-  const [randomPrompt, setRandomPrompt] = useState<string>(""); // Typing the state as string
+  const [randomPrompt, setRandomPrompt] = useState<string>("");
+  const [isRecording, setIsRecording] = useState<boolean>(false); // State to track if recording is active
 
   const prompts = [
-    'When was the last time you felt speechless? Tell us about it.',
-    'When was the last time you felt completely enchanted by something? Tell us about it.',
-    'Have you had an unexpected encounter with a stranger at Karneval that left you with a feeling of curiosity? Tell us about it.',
-    'Consider a time at Karneval when you felt a deep connection to something greater than yourself. Tell us about it.'
+    'When was the last time you felt speechless? Tell me about it.',
+    'When was the last time you felt completely enchanted by something? Tell me about it.',
+    'Tell me about an experience where you felt a deep sense of gratitude and appreciation for the simple things in life.',
+    'Share a time when you were moved to tears by an act of kindness, generosity, or compassion.',
+    'Have you ever encountered a new idea or concept that challenged your existing beliefs and prompted you to reevaluate your worldview? Tell me about it.',
+    'Share a time when you were awestruck by the power of nature.',
+    'Describe a time when you felt a deep sense of connection with a place, culture, or tradition that was new to you, and how it expanded your perspective.',
+    'Have you had an unexpected encounter with a stranger at Karneval that left you with a feeling of curiosity? Tell me about it.',
+    'Has there been a moment at Karneval when you felt a deep connection to something greater than yourself? Tell me about it.'
   ];
 
-  const getRandomPrompt = (): string => {  // Ensure function return type is string
+  const getRandomPrompt = (): string => {
     return prompts[Math.floor(Math.random() * prompts.length)];
   };
 
-  const proceed = (nextPhase: string): void => { // Ensure proper typing for parameters
+  const proceed = (nextPhase: string): void => {
     setPhase(nextPhase);
     if (nextPhase === "karneval") {
-      setRandomPrompt(getRandomPrompt()); // Update the prompt when entering 'karneval'
+      setRandomPrompt(getRandomPrompt());
     }
   };
 
@@ -33,17 +39,16 @@ const SplashScreen: React.FC<{ onEnter: (prompt?: string) => void }> = ({
     exit: { opacity: 0, transition: { duration: 1.5, ease: "easeInOut" } },
   };
 
-
-
   useEffect(() => {
     if (phase === "welcome") {
       // Removed the automatic transition to "learnMore" phase
     }
   }, [phase]);
-  
+
   const onRecordingComplete = async (audioBlob) => {
+    setIsRecording(false); // Stop animation when recording is complete
     const formData = new FormData();
-    formData.append("audio", audioBlob, "audio.wav"); // 'audio' is the field name expected by the server
+    formData.append("audio", audioBlob, "audio.wav");
 
     try {
       const response = await fetch('/api/transcribe', {
@@ -56,14 +61,16 @@ const SplashScreen: React.FC<{ onEnter: (prompt?: string) => void }> = ({
       }
 
       const data = await response.json();
-      console.log('Transcription:', data.transcription); // Log the transcription to the console
+      console.log('Transcription:', data.transcription);
 
-      // Proceed to chat page with the transcription text
       onEnter(data.transcription);
     } catch (error) {
       console.error('Error uploading audio:', error);
-      // Handle the error state appropriately, maybe show a message to the user
     }
+  };
+
+  const startRecording = () => {
+    setIsRecording(true); // Start animation when recording starts
   };
 
   return (
@@ -74,6 +81,13 @@ const SplashScreen: React.FC<{ onEnter: (prompt?: string) => void }> = ({
       exit="exit"
       variants={variants}
     >
+      {isRecording && (
+        <div className="recordingBackground">
+          <div className="waveLine"></div>
+          <div className="waveLine"></div>
+          <div className="waveLine"></div>
+        </div>
+      )}
       {phase === "welcome" && (
         <motion.div className="content" variants={variants}>
           <h1 className="gradientText">Welcome to Loob</h1>
@@ -94,37 +108,41 @@ const SplashScreen: React.FC<{ onEnter: (prompt?: string) => void }> = ({
         </motion.div>
       )}
 
-{phase === "karneval" && (
-  <motion.div className="content" variants={variants}>
-    <h1 className="gradientText">Today&apos;s theme: sit, sip, speak.</h1>
-    <h2 className="gradientText">{randomPrompt}</h2> {/* This will now display the random prompt */}
-    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '20px' }}>
-      <AudioRecorder onRecordingComplete={onRecordingComplete} />
-    </div>
-  </motion.div>
-)}
+      {phase === "karneval" && (
+        <motion.div className="content" variants={variants}>
+          <h2 className="gradientText">{randomPrompt}</h2>
+          <div className="buttonContainer">
+            <AudioRecorder
+              onRecordingComplete={onRecordingComplete}
+              startRecording={startRecording}
+            />
+            <button className="newPromptButton" onClick={() => setRandomPrompt(getRandomPrompt())}>
+              New Prompt
+            </button>
+          </div>
+        </motion.div>
+      )}
 
-
-    {phase === "learnMore" && (
-  <motion.div className="content" variants={variants}>
-    <h1 className="gradientText">Would you like to</h1>
-    <button onClick={() => onEnter("I would like to talk about my night with you.")}>
-      Share about your night
-    </button>
-<h3 className="gradientText" style={{ fontSize: 'normal' }}><br/>or learn more about</h3>
-    <div className="buttonContainer">
-      <button className="smallButton" onClick={() => onEnter("Tell me about MOOS.")}>
-        MOOS
-      </button>
-      <button className="smallButton" onClick={() => onEnter("Tell me about EDS and seks/loob.")}>
-        EDS
-      </button>
-      <button className="smallButton" onClick={() => onEnter("I am having a difficult time, can you give me harm reduction support")}>
-        Harm Reduction
-      </button>
-    </div>
-  </motion.div>
-)}
+      {phase === "learnMore" && (
+        <motion.div className="content" variants={variants}>
+          <h1 className="gradientText">Would you like to</h1>
+          <button onClick={() => onEnter("I would like to talk about my night with you.")}>
+            Share about your night
+          </button>
+          <h3 className="gradientText" style={{ fontSize: 'normal' }}><br/>or learn more about</h3>
+          <div className="buttonContainer">
+            <button className="smallButton" onClick={() => onEnter("Tell me about MOOS.")}>
+              MOOS
+            </button>
+            <button className="smallButton" onClick={() => onEnter("Tell me about EDS and seks/loob.")}>
+              EDS
+            </button>
+            <button className="smallButton" onClick={() => onEnter("I am having a difficult time, can you give me harm reduction support")}>
+              Harm Reduction
+            </button>
+          </div>
+        </motion.div>
+      )}
 
       {phase === "feedback" && (
         <motion.div className="content" variants={variants}>
@@ -149,8 +167,6 @@ const SplashScreen: React.FC<{ onEnter: (prompt?: string) => void }> = ({
           </button>
         </motion.div>
       )}
-
-      {/* Ensure other phases are handled as they were, without introducing new functionality or variables */}
     </motion.div>
   );
 };
