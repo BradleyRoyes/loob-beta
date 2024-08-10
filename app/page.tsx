eb6a7b70611977ca07c061a5677dec2cc8771196
seks.design <bradroyes@gmail.com>
2024-08-10 19:02:31 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -173,7 +173,7 @@ export default function Page() {
             <div className="flex justify-between items-center">
               <h1 className="chatbot-text-primary text-6xl md:text-7xl font-extrabold tracking-wide">
                 <span className="text-5xl md:text-7xl">loob</span>
-                <span className="text-lg md:text-xl font-normal"> at EDS </span>
+                <span className="text-lg md:text-xl font-normal">at EDS</span>
               </h1>
               <div className="flex gap-1">
                 <ThemeButton />
