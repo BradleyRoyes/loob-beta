0ada83660212beff4a43b6c3f38d78d6d0233625
seks.design <bradroyes@gmail.com>
2024-11-15 14:52:14 +0100

    Update AudioRecorder.tsx

--- components/AudioRecorder.tsx
+++ components/AudioRecorder.tsx
@@ -166,7 +166,7 @@ const AudioRecorder: React.FC<AudioRecorderProps> = ({ onRecordingComplete, star
         <rect x="5" y="5" width="14" height="14" fill="#d32f2f" rx="3" />
       ) : (
         <>
-          <path d="M12 1a3 3 0 0 1 3 3v6a3 3 0 0 1-6 0V4a3 3 0 0 1 3-3z" />
+          <path d="M12 1a3 3 0 0 1 3 3v6a3 3 0 0 1-6 0V4a3 3 0 1 1 3-3z" />
           <path d="M19 10v2a7 7 0 0 1-14 0v-2" />
           <line x1="12" y1="19" x2="12" y2="23" />
           <line x1="8" y1="23" x2="16" y2="23" />
@@ -186,11 +186,13 @@ const AudioRecorder: React.FC<AudioRecorderProps> = ({ onRecordingComplete, star
       >
         <MicIcon />
       </button>
+      {/* 
       {recording && (
         <div className="recordingIndicator">
           <p>Recording... {timer}s</p>
         </div>
       )}
+      */}
     </div>
   );
 };
